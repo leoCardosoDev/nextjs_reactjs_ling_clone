4fb8557f2d20ad55eec700f13967153f2e0f6a7a
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-10 17:00:19 -0300

    feat: add inital page with signin and signup

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,51 @@
+import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <p>This is markenting page</p>
+    <div className="max-w-[980px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h=[424px] mb-8 lg:mb-0">
+        <Image src="/hero.svg" alt="Hero" fill />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-lg lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Aprenda, pratique e domine novos idiomas com Lingo
+        </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal" forceRedirectUrl="/learn">
+                <Button size="lg" variant="secondary" className="w-full">
+                  Vamos começar!
+                </Button>
+              </SignUpButton>
+              <SignInButton mode="modal" forceRedirectUrl="/learn">
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  Já tennho uma conta!
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">Continue aprendendo</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
     </div>
   );
 }
