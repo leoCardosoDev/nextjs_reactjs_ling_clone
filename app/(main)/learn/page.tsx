2f9adb816f221113a6207bdbf48807024fc7c0d6
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-11 18:01:12 -0300

    feat: add FeddWrapper

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,14 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
 
 const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>My stick sider here</StickyWrapper>
+      <FeedWrapper>
+        <Header title="Espanhol" />
+      </FeedWrapper>
     </div>
   );
 };
