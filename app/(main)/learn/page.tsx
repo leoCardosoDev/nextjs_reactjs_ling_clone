34966dd569ec81f321bfd8a11e06bbdc00026da3
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-11 18:47:37 -0300

    feat: add sitcky header and userprogress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,11 +1,19 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
 
 const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
-      <StickyWrapper>My stick sider here</StickyWrapper>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Espanhol", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={true}
+        />
+      </StickyWrapper>
       <FeedWrapper>
         <Header title="Espanhol" />
       </FeedWrapper>
