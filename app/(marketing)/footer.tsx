f8fef8d85d5540bcfe9271b2d7f0dff011ea1d5f
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-11 14:50:52 -0300

    feat: finish welcomepage

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,41 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+type LanguageButtonProps = {
+  src: string;
+  alt: string;
+  label: string;
+};
+
+const languages = [
+  { src: "/hr.svg", alt: "Croatian", label: "Croata" },
+  { src: "/es.svg", alt: "Spanish", label: "Espanhol" },
+  { src: "/fr.svg", alt: "French", label: "Françês" },
+  { src: "/it.svg", alt: "Italian", label: "Italiano" },
+  { src: "/jp.svg", alt: "Japanese", label: "Japonês" },
+];
+
+const LanguageButton: React.FC<LanguageButtonProps> = ({ src, alt, label }) => (
+  <Button size="lg" variant="ghost" className="w-full">
+    <Image
+      src={src}
+      alt={alt}
+      height={32}
+      width={40}
+      className="mr-4 rounded-md"
+    />
+    {label}
+  </Button>
+);
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        {languages.map((lang) => (
+          <LanguageButton key={lang.alt} {...lang} />
+        ))}
+      </div>
     </footer>
   );
 };
