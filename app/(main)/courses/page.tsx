628f7bb823b8e57a0b4806bac136fea0ac515214
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-12 14:17:38 -0300

    feat: add layout to front courses

---
+++ app/(main)/courses/page.tsx
@@ -0,0 +1,14 @@
+import { getCourses } from "@/db/queries";
+import { List } from "./list";
+
+const CoursesPage = async () => {
+  const courses = await getCourses();
+  return (
+    <div className="h-full max-w-[912px] px-3 mx-auto">
+      <h1 className="text-2xl font-bold text-neutral-700">Cursos de idiomas</h1>
+      <List courses={courses} activeCoursedId={1} />
+    </div>
+  );
+};
+
+export default CoursesPage;
