71e218e947c9e3581d243e8bd04afeda34901b6a
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-13 20:09:12 -0300

    feat: add load courses progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,17 +1,22 @@
+import { redirect } from "next/navigation";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+  const [userProgress] = await Promise.all([userProgressData]);
+  if (!userProgress || !userProgress.activeCourse) redirect("/courses");
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "Espanhol", imageSrc: "/es.svg" }}
-          hearts={5}
-          points={100}
-          hasActiveSubscription={true}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={userProgress.activeCourseId ? true : false}
         />
       </StickyWrapper>
       <FeedWrapper>
