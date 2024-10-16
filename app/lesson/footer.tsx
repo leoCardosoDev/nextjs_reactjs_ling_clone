8644e658b8d9194f02af92d3f136914f7d0addf0
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-16 16:58:53 -0300

    feat: finalize footer

---
+++ app/lesson/footer.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { CheckCircle, XCircle } from "lucide-react";
+import { useKey, useMedia } from "react-use";
+
+type Props = {
+  status: "correct" | "wrong" | "none" | "completed";
+  onCheck: () => void;
+  disabled?: boolean;
+  lessonId?: boolean;
+};
+
+export const Footer = ({ status, disabled, onCheck, lessonId }: Props) => {
+  useKey("Enter", onCheck, {}, [onCheck]);
+  const isMobile = useMedia("(max-width: 1024)");
+  return (
+    <footer
+      className={cn(
+        "lg:h-[140px] h-[100px] border-t-2",
+        status === "correct" && "border-transparent bg-green-100",
+        status === "wrong" && "border-transparent bg-rose-100"
+      )}
+    >
+      <div className="max-w-[1140px] h-full mx-auto flex items-center justify-between px-6 lg:px-10">
+        {status === "correct" && (
+          <div className="text-green-500 font-bold text-base lg:text-2xl flex items-center">
+            <CheckCircle className="h-6 w-6 lg:h-10 lg:w-10 mr-4" />
+            Parabéns!
+          </div>
+        )}
+        {status === "wrong" && (
+          <div className="text-rose-500 font-bold text-base lg:text-2xl flex items-center">
+            <XCircle className="h-6 w-6 lg:h-10 lg:w-10 mr-4" />
+            Tente outra vez!
+          </div>
+        )}
+        {status === "completed" && (
+          <Button
+            variant="default"
+            size={isMobile ? "sm" : "lg"}
+            onClick={() => (window.location.href = `/lesson/${lessonId}`)}
+          >
+            Praticar outra vez
+          </Button>
+        )}
+        <Button
+          disabled={disabled}
+          className="ml-auto"
+          onClick={onCheck}
+          size={isMobile ? "sm" : "lg"}
+          variant={status === "wrong" ? "danger" : "secondary"}
+        >
+          {status === "none" && "Confirmar"}
+          {status === "correct" && "Próxima"}
+          {status === "wrong" && "Reenviar"}
+          {status === "completed" && "Continuar"}
+        </Button>
+      </div>
+    </footer>
+  );
+};
