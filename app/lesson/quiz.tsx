91d741d86a40e4dcda32c1aae4f2661167d6dc39
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-16 09:21:20 -0300

    feat: add initial page for lesson

---
+++ app/lesson/quiz.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { challengeOptions, challenges } from "@/db/schema";
+
+type Props = {
+  initialLessonId: number;
+  initialLessonChallenges: (typeof challenges.$inferSelect & {
+    completed: boolean;
+    challengeOptions: (typeof challengeOptions.$inferSelect)[];
+  })[];
+  initialHearts: number;
+  initialPercentage: number;
+  userSubscription: undefined;
+};
+
+export const Quiz = ({
+  initialHearts,
+  initialLessonChallenges,
+  initialLessonId,
+  initialPercentage,
+  userSubscription,
+}: Props) => {
+  return <div>Quiz</div>;
+};
