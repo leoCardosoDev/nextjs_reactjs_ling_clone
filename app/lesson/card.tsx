bf169ec009c8ea3c5da65f1155d3863941a5770f
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-16 15:50:04 -0300

    feat: finalize card

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -1,4 +1,6 @@
 import { challenges } from "@/db/schema";
+import { useCallback } from "react";
+import { useAudio, useKey } from "react-use";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
@@ -23,10 +25,21 @@ export const Card = ({
   imageSrc,
   text,
   shortcut,
+  onClick,
+  audioSrc,
 }: Props) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [audio, _, controls] = useAudio({ src: audioSrc || "" });
+  const handleClick = useCallback(() => {
+    if (disabled) return;
+    controls.play();
+    onClick();
+  }, [disabled, onClick, controls]);
+  useKey(shortcut, handleClick, {}, [handleClick]);
+
   return (
     <div
-      onClick={() => {}}
+      onClick={handleClick}
       className={cn(
         "h-full border-2 rounded-xl border-b-4 hover:bg-black/50 p-4 lg:p-6 cursor-pointer active:border-b-2",
         selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
@@ -40,6 +53,7 @@ export const Card = ({
         type === "ASSIST" && "lg:p-3 w-full"
       )}
     >
+      {audio}
       {imageSrc && (
         <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
           <Image src={imageSrc} fill alt={text} />
