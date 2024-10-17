7983f8c7ee629953d010f9c1557ea9ca487f6b5b
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-17 09:52:33 -0300

    feat: add quiz page

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,6 +1,15 @@
 "use client";
 
+import { useState, useTransition } from "react";
 import { challengeOptions, challenges } from "@/db/schema";
+import { Header } from "./header";
+import { QuestionBubble } from "./question-bubble";
+import { Challenge } from "./challenge";
+import { Footer } from "./footer";
+import { upsertChallengeProgress } from "@/actions/challenge-progress";
+import { toast } from "sonner";
+import { reduceHearts } from "@/actions/user-progress";
+import { useAudio } from "react-use";
 
 type Props = {
   initialLessonId: number;
@@ -10,15 +19,134 @@ type Props = {
   })[];
   initialHearts: number;
   initialPercentage: number;
-  userSubscription: undefined;
+  userSubscription: any;
 };
 
 export const Quiz = ({
+  initialLessonId,
   initialHearts,
   initialLessonChallenges,
-  initialLessonId,
   initialPercentage,
   userSubscription,
 }: Props) => {
-  return <div>Quiz</div>;
+  const [correctAudio, _c, correctControls] = useAudio({
+    src: "/correct.wav",
+  });
+  const [inCorrectAudio, _i, inCorrectControls] = useAudio({
+    src: "/incorrect.wav",
+  });
+  const [pending, startTransition] = useTransition();
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+  const [selectedOption, setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
+  const onSelect = (id: number) => {
+    if (status !== "none") return;
+    setSelectedOption(id);
+  };
+  const onNext = () => {
+    setActiveIndex((current) => current + 1);
+  };
+  const onContinue = () => {
+    if (!selectedOption) return;
+    if (status === "wrong") {
+      setStatus("none");
+      setSelectedOption(undefined);
+      return;
+    }
+    if (status === "correct") {
+      onNext();
+      setStatus("none");
+      setSelectedOption(undefined);
+      return;
+    }
+    const correctOption = options.find((option) => option.correct);
+    if (!correctOption) return;
+    if (correctOption.id === selectedOption) {
+      startTransition(() => {
+        upsertChallengeProgress(challenge.id)
+          .then((response) => {
+            if (response?.error === "hearts") {
+              console.error("Missing hearts");
+              return;
+            }
+            correctControls.play();
+            setStatus("correct");
+            setPercentage((prev) => prev + 100 / challenges.length);
+            if (initialPercentage === 100) {
+              setHearts((prev) => Math.min(prev + 1, 5));
+            }
+          })
+          .catch(() => toast.error("Algo errado aconteceu! Tente novamente"));
+      });
+    } else {
+      startTransition(() => {
+        reduceHearts(challenge.id)
+          .then((response) => {
+            if (response?.error === "hearts") {
+              console.error("Missing hearts");
+              return;
+            }
+            inCorrectControls.play();
+            setStatus("wrong");
+            if (!response?.error) {
+              setHearts((prev) => Math.max(prev - 1, 0));
+            }
+          })
+          .catch(() => toast.error("Algo errado aconteceu! Tente novamente"));
+      });
+    }
+  };
+  const title =
+    challenge.type === "ASSIST"
+      ? "Selecione o significado correto:"
+      : challenge.question;
+
+  return (
+    <>
+      {correctAudio}
+      {inCorrectAudio}
+      <Header
+        hearts={hearts}
+        percentage={percentage}
+        hasActiveSubscription={!!userSubscription?.isActive}
+      />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+              <Challenge
+                options={options}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
+                disabled={pending}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer
+        disabled={pending || !selectedOption}
+        status={status}
+        onCheck={onContinue}
+      />
+    </>
+  );
 };
